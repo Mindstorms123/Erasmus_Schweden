d1dcd2429d78b890edd723fa3a524a1c3514ae39
Mindstorms123 <144943256+Mindstorms123@users.noreply.github.com>
2025-04-01 15:56:44 +0200

    Create test.py

---
+++ test.py
@@ -0,0 +1,13 @@
+// test for the client liberary
+
+from clientlib import GreifarmClient
+client = GreifarmClient("Put IP Address here")
+
+response=client.send_servo_position(x=120, y=150, z=200) // Werte von 0 bis 270
+print("Servo response", response)
+
+response2 = client.toggle_magnet(TRUE) // TRUE oder FALSE
+print("Magnet response", response2)
+
+response3 = client.shutdown_server()
+print("Server shutdown", response3)
