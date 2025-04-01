3886b29bba38e23973b62eb35364f65a71cfaee1
Mindstorms123 <144943256+Mindstorms123@users.noreply.github.com>
2025-04-01 15:59:24 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,12 +1,12 @@
-// test for the client liberary
+# test for the client liberary
 
 from clientlib import GreifarmClient
 client = GreifarmClient("Put IP Address here")
 
-response=client.send_servo_position(x=120, y=150, z=200) // Werte von 0 bis 270
+response=client.send_servo_position(x=120, y=150, z=200) # Werte von 0 bis 270
 print("Servo response", response)
 
-response2 = client.toggle_magnet(TRUE) // TRUE oder FALSE
+response2 = client.toggle_magnet(TRUE) # TRUE oder FALSE
 print("Magnet response", response2)
 
 response3 = client.shutdown_server()
